1333aaacf2e7372cd3e37e199c22f033f4a20d0a
Kuldeep Kumar <118204485+Kuldeep-Angural@users.noreply.github.com>
2024-07-22 10:57:59 +0530

    f

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ export const createObject = async (queryString) => {
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
+        console.log("response",response);
         return response.json(); // Assuming response is JSON data
     } catch (error) {
         console.error('Error fetching data:', error);
